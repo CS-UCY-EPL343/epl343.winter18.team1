f0df0b49d29360e45fea6ba7906667e3a00bd7d5
pavlinak <31859267+pavlinak@users.noreply.github.com>
2019-04-17 12:27:24 +0300

    Update ArticlesController.php

--- Controllers/ArticlesController.php
+++ Controllers/ArticlesController.php
@@ -49,7 +49,6 @@ class ArticlesController extends Controller
     {
         $list = \DB::table('ARTICLE')->pluck('ARTICLE_ID')->toArray();
         $fn1 = $list[count($list)-1] + 1;
-        $fn2 = "cl";
 
         $ARTICLE_ID = $fn1;
 
@@ -65,27 +64,6 @@ class ArticlesController extends Controller
         $data = array('ARTICLE_ID'=>$ARTICLE_ID,'DOCUMENT'=>$DOCUMENT,'TITLE'=>$TITLE,'IMAGE'=>$IMAGE,'DATE'=>$DATE);
 
         \DB::table('ARTICLE')->insert($data);
-
-
-        //if "email" variable is filled out, send email
-        // if (isset($_REQUEST['EMAIL']))  {
-          
-        function remove_headers($string) {
-        $headers = array(
-            "/to\:/i",
-            "/from\:/i",
-            "/bcc\:/i",
-            "/cc\:/i",
-            "/Content\-Transfer\-Encoding\:/i",
-            "/Content\-Type\:/i",
-            "/Mime\-Version\:/i"
-            );
-            if (preg_replace($headers, '', $string) == $string) {
-            return $string;
-            } else {
-            die('You are not completing the form correctly.');
-            }
-        }
 
         return redirect()->back()->with('alert', 'Request submited!');
     }
